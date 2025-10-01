e111ff362cba1d5ab8a0c6652913c83e3ad605f8
hanum <dlgksdma15@naver.com>
2025-10-01 15:41:36 +0900

    todo15

--- src/main/java/com/nhnacademy/shoppingmall/common/initialize/PointThreadInitializer.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/initialize/PointThreadInitializer.java
@@ -2,23 +2,37 @@ package com.nhnacademy.shoppingmall.common.initialize;
 
 import com.nhnacademy.shoppingmall.thread.channel.RequestChannel;
 import com.nhnacademy.shoppingmall.thread.worker.WorkerThread;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import java.util.Set;
 
+@Slf4j
 public class PointThreadInitializer implements ServletContainerInitializer {
 
     @Override
     public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
 
         RequestChannel requestChannel = new RequestChannel(10);
+
         //todo#14-1 servletContext에 requestChannel을 등록합니다.
 
+        ctx.setAttribute("requestChannel", requestChannel);
+        log.info("RequestChannel registered in servlet context");
 
         //todo#14-2 WorkerThread 사작합니다.
 
+        int workerCount = 5;
+
+        for (int i = 0; i < workerCount; i++) {
+            WorkerThread worker = new WorkerThread(requestChannel);
+            worker.setName("WorkerThread-" + (i + 1));
+            worker.start();
+            log.info("{} started.",worker.getName());
+
+        }
 
     }
 }
--- src/main/java/com/nhnacademy/shoppingmall/thread/request/impl/PointChannelRequest.java
+++ src/main/java/com/nhnacademy/shoppingmall/thread/request/impl/PointChannelRequest.java
@@ -2,6 +2,8 @@ package com.nhnacademy.shoppingmall.thread.request.impl;
 
 import com.nhnacademy.shoppingmall.common.mvc.transaction.DbConnectionThreadLocal;
 import com.nhnacademy.shoppingmall.thread.request.ChannelRequest;
+import com.nhnacademy.shoppingmall.user.domain.User;
+import com.nhnacademy.shoppingmall.user.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.SQLException;
@@ -9,11 +11,41 @@ import java.sql.SQLException;
 @Slf4j
 public class PointChannelRequest extends ChannelRequest {
 
+    private final UserRepository userRepository;
+    private final String userId;
+    private final int points;
+
+
+    public PointChannelRequest(UserRepository userRepository, String userId, int points) {
+        this.userRepository = userRepository;
+        this.userId = userId;
+        this.points = points;
+    }
+
     @Override
     public void execute() throws SQLException {
         DbConnectionThreadLocal.initialize();
+
         //todo#14-5 포인트 적립구현, connection은 point적립이 완료되면 반납합니다.
+        User user = userRepository.findById(userId).orElse(null);
+        try {
+            if (user != null) {
+                // 사용자의 현재 포인트에 적립할 포인트 더한다.
+                user.setUserPoint(user.getUserPoint() + points);
+
+                // 변경된 사용자 정보로 업데이트
+                userRepository.update(user);
+                log.info("유저 정보 변경: " + user);
+            } else {
+                log.warn("유저 아이디를 찾을 수 없습니다. userId:{}", userId);
+            }
+        } catch (Exception e){
+            log.error("포인트가 추가 되지 못해 롤백합니다." + e.getMessage());
+            DbConnectionThreadLocal.setSqlError(true);
+            throw new RuntimeException(e);
+        } finally {
+            DbConnectionThreadLocal.reset();
+        }
         log.debug("pointChannel execute");
-        DbConnectionThreadLocal.reset();
     }
 }
