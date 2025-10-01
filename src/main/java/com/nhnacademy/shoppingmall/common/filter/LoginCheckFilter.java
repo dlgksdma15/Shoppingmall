9f91a95b8a0ad6fdac41a38fd29c4ef5d50cecc3
hanum <dlgksdma15@naver.com>
2025-10-01 13:29:47 +0900

    todo9

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/LoginCheckFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/LoginCheckFilter.java
@@ -7,13 +7,29 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Slf4j
+@WebFilter(
+        filterName = "loginCheckFilter",
+        urlPatterns = "/mypage/*"
+)
 public class LoginCheckFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //todo#10 /mypage/ 하위경로의 접근은 로그인한 사용자만 접근할 수 있습니다.
+
+        HttpSession session = req.getSession(false);
+
+        if(session == null || session.getAttribute("user") == null){
+            // 로그인 안됨
+            res.sendRedirect(req.getContextPath() + "/login.do");
+            return;
+        }
+
+        // 로그인 됨 -> 다음 필터/서블릿으로 진행
+        chain.doFilter(req,res);
 
     }
 }
