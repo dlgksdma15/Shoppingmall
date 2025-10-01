221785bff9437cb115dd8ffd3c616d26dcc5efca
hanum <dlgksdma15@naver.com>
2025-10-01 14:40:12 +0900

    todo13,14

--- src/main/java/com/nhnacademy/shoppingmall/controller/auth/LoginController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/auth/LoginController.java
@@ -5,6 +5,7 @@ import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @RequestMapping(method = RequestMapping.Method.GET,value = "/login.do")
 public class LoginController implements BaseController {
@@ -13,6 +14,13 @@ public class LoginController implements BaseController {
     public String execute(HttpServletRequest req, HttpServletResponse resp) {
         //todo#13-1 session이 존재하고 로그인이 되어 있다면 redirect:/index.do 반환 합니다.
 
+        HttpSession session = req.getSession();
+
+        if(session != null || session.getAttribute("user") != null){
+            return "redirect:/index.do";
+        }
+
+        // 로그인이 안되었으면 로그인폼으로 이동
         return "shop/login/login_form";
     }
 }
--- src/main/java/com/nhnacademy/shoppingmall/controller/auth/LogoutController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/auth/LogoutController.java
@@ -1,5 +1,27 @@
 package com.nhnacademy.shoppingmall.controller.auth;
 
-public class LogoutController {
+import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
+import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@RequestMapping(method = RequestMapping.Method.GET, value = "/logout.do")
+public class LogoutController implements BaseController {
+
     //todo#13-3 로그아웃 구현
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) {
+
+        HttpSession session = req.getSession();
+        if(session != null){
+            session.invalidate();
+        }
+
+        return "redirect:/index.do";
+    }
+
+
+
 }
